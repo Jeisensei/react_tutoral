acd742fbf82d0e7303836db6abff8c8d041ee45f
Jason Grant <69483935+Jeisensei@users.noreply.github.com>
2023-05-30 07:00:35 +0000

    update

--- 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
+++ 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 import data from "./data";
 const App = () => {
-  return <h2>{data}</h2>;
+  const [person, setPerson] = useState(data.name);
+  return <>
+    <h2>testing</h2>
+    <h4>{data.length}</h4>
+  </>
 };
 export default App;
