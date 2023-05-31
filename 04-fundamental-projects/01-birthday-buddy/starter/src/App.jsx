fedc84d2c496cfd26f5b576385f2de02cdbd546e
Jason Grant <69483935+Jeisensei@users.noreply.github.com>
2023-05-31 02:38:08 +0000

    following the tutoral

--- 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
+++ 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import data from "./data";
+import List from "./List";
 
 const App = () => {
   const [person, setPerson] = useState(data);
@@ -7,23 +8,40 @@ const App = () => {
     setPerson([]);
   };
   return (
-    <>
-      {data.length === 1 ? (
-        <h1>There is 1 birthday today.</h1>
-      ) : (
-        <h1>There are {person.length} birthdays today</h1>
-      )}
-      {person.map((onePerson) => {
-        return (
-          <div key={onePerson.id}>
-            <h2>{onePerson.name}</h2>
-          </div>
-        );
-      })}
-      <button type="button" onClick={handleClick}>
-        Clear all
-      </button>
-    </>
+    // <>
+    //   {data.length === 1 ? (
+    //     <h1>There is 1 birthday today.</h1>
+    //   ) : (
+    //     <h1>There are {person.length} birthdays today</h1>
+    //   )}
+    //   {person.map((onePerson) => {
+    //     return (
+    //       <ul key={onePerson.id}>
+    //         <li>
+    //           <img src={onePerson.image} alt={onePerson.name}></img>
+    //         </li>
+    //         <li>{onePerson.name}</li>
+    //         <li>{onePerson.age}</li>
+    //       </ul>
+    //     );
+    //   })}
+    //   <button type="button" onClick={handleClick}>
+    //     Clear all
+    //   </button>
+    // </>
+    <main>
+      <section className="container">
+        {person.length === 1 ? (
+          <h3>1 birthday today</h3>
+        ) : (
+          <h3>{person.length} birthdays today</h3>
+        )}
+        <List people={person} />
+        <button type="button" className="btn btn-block" onClick={handleClick}>
+          Clear all
+        </button>
+      </section>
+    </main>
   );
 };
 export default App;
