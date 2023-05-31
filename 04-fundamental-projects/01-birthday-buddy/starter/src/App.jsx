0fb106db13548cfb762e118e69af5c855891b0c2
Jason Grant <69483935+Jeisensei@users.noreply.github.com>
2023-05-31 00:27:16 +0000

    Getting closer to a finished project

--- 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
+++ 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
@@ -1,10 +1,29 @@
 import { useState } from "react";
 import data from "./data";
+
 const App = () => {
-  const [person, setPerson] = useState(data.name);
-  return <>
-    <h2>testing</h2>
-    <h4>{data.length}</h4>
-  </>
+  const [person, setPerson] = useState(data);
+  const handleClick = () => {
+    setPerson([]);
+  };
+  return (
+    <>
+      {data.length === 1 ? (
+        <h1>There is 1 birthday today.</h1>
+      ) : (
+        <h1>There are {person.length} birthdays today</h1>
+      )}
+      {person.map((onePerson) => {
+        return (
+          <div key={onePerson.id}>
+            <h2>{onePerson.name}</h2>
+          </div>
+        );
+      })}
+      <button type="button" onClick={handleClick}>
+        Clear all
+      </button>
+    </>
+  );
 };
 export default App;
