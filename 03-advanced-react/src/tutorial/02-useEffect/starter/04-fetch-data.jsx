6c53373cffb9685b52fc3dfc9390f4a62afa7798
Jason Grant <69483935+Jeisensei@users.noreply.github.com>
2023-06-04 04:12:37 +0000

    updating ipad

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,14 @@
-const url = 'https://api.github.com/users';
+import { useState, useEffect } from "react";
+
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => console.log(data));
+  });
   return <h2>fetch data example</h2>;
 };
 export default FetchData;
